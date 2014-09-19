ebf76dd86656e05c4a1c25df6b30952939438b30
Dustin <dustin@pdx.edu>
2014-09-19 13:18:25 -0700

    Add full package list for this module

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,16 @@
 #
 class ldapserver::install {
 
-  package { '389ds':
-    ensure => 'installed'
-  }
-
+ensure_packages (['389-ds-base',
+            '389-ds-console-doc',
+            '389-adminutil',
+            '389-ds-base-libs',
+            '389-admin',
+            '389-ds-console',
+            '389-admin-console-doc',
+            '389-ds',
+            '389-console',
+            '389-admin-console',
+            '389-dsgw',
+            ])
 }
