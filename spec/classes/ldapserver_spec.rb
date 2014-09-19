6b36c8dc5d9e5d74920f93b8305eadbc22ce3b1f
Dustin <dustin@pdx.edu>
2014-09-19 13:50:26 -0700

    Update the tests

--- spec/classes/ldapserver_spec.rb
+++ spec/classes/ldapserver_spec.rb
@@ -4,6 +4,5 @@ describe 'ldapserver' do
   it { should compile }
   it { should contain_class('ldapserver::install') }
   it { should contain_class('ldapserver::service') }
-  it { should contain_class('ldapserver::params') }
 
 end
